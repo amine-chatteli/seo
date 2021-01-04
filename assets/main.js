29433cc4f3c181d8bf248c7c5327e53921b71b77
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2021-01-04 19:48:28 +0100

    seo initial commit

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+//  document.getElementsByClassName('mobile')[0]
+//     .addEventListener('click', () => {
+//         document.getElementsByClassName('mobile__navbar')[0].style.top = '0'
+//     })
+
+// let btns = document.getElementsByClassName('close')
+// let btnsArr = Array.prototype.slice.call(btns);
+// btnsArr.forEach(element => {
+//     element.addEventListener('click', () => {
+//         document.getElementsByClassName('mobile__navbar')[0].style.top = '-100%'
+//     })
+// }); 
+
+   
